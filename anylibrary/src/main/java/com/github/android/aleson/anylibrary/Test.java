c411cfa3bbb4df28dbb4297bd1b9fde06da1c638
Aleson Alves <aleson.a.s@gmail.com>
2018-12-16 17:17:42 -0200

    test jitpack

---
+++ anylibrary/src/main/java/com/github/android/aleson/anylibrary/Test.java
@@ -0,0 +1,11 @@
+package com.github.android.aleson.anylibrary;
+
+import android.util.Log;
+
+public class Test {
+
+    public Test(){
+        Log.d("LOG", "LOGGING");
+    }
+
+}
--- app/src/main/java/com/github/android/aleson/jitpackpoc/MainActivity.java
+++ app/src/main/java/com/github/android/aleson/jitpackpoc/MainActivity.java
@@ -3,11 +3,14 @@ package com.github.android.aleson.jitpackpoc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.android.aleson.anylibrary.Test;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Test();
     }
 }
